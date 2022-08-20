205a6d6dfd655aa446d3369c4528529c09986dcf
trokeibunor <okeibunoremma@gmail.com>
2022-08-20 20:15:20 +0100

    finished ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Body from './components/Body';
 function App() {
   return (
     <div className="App  flex w-100 h-screen bg-slate-300">
       <div className="content w-5/12 h-auto m-auto bg-white">
         <Header/>
+        <Body/>
       </div>
     </div>
   );
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,28 @@
 import React from "react";
-import '../style/body.css'
+import '../style/body.css';
+import MemeImg from "../images/memeimg.png"
 export default class Body  extends React.Component{
     render(){
         return(
-            <div>
-
+            <div className="cardBody">
+                <section className="form w-10/12 mx-auto flex flex-col">
+                    <form className="flex flex-col w-full">
+                        <div className="input_row flex flex-row w-full justify-between">
+                            <input 
+                                type="text" 
+                                className="w-5/12 px-6 py-2 my-4 outline-none" 
+                                placeholder="Top Text"/>
+                            <input 
+                                type="text" 
+                                className="w-5/12 px-6 py-2 my-4 outline-none" 
+                                placeholder="Bottom Text"/>
+                        </div>
+                        <button className="submitBtn w-12/12 my-2 text-center" type="submit">Get new meme Image</button>
+                    </form>
+                    <div className="imgBox w-full my-8">
+                        <img src={MemeImg} className="h-72 w-full" alt=""/>
+                    </div>
+                </section>
             </div>
         )
     }
